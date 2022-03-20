2f4673bf21c8b5252d0d9b74169f1ac1d0f88d19
pjazdzyk <piotr.jazdzyk@o2.pl>
2022-03-20 13:39:00 +0100

    add missing tests for ProcAirHeatCoolTests

--- src/test/java/Model/ProcAirHeatCoolTests.java
+++ src/test/java/Model/ProcAirHeatCoolTests.java
@@ -14,7 +14,7 @@ public class ProcAirHeatCoolTests {
     static final double MATH_ACCURACY = LibDefaults.DEF_MATH_ACCURACY;
 
    @Test
-   void ProcAirHeatCoolConstructorTests(){
+   void procAirHeatCoolConstructorTest(){
        //Arrange
        var expectedFlowMa = 5000; //m3/h
        var expectedInletFlow = FlowOfMoistAir.ofM3hVolFlow(5000,-20,100);
@@ -38,12 +38,40 @@ public class ProcAirHeatCoolTests {
 
    }
 
+   // HEATING
    @Test
-   void applyHeatingInQFromOutTxTests(){
+   void applyHeatingOutTxFromInQTest(){
+
+        // ARRANGE
+        var winterInletFlow = FlowOfMoistAir.ofM3hVolFlow("InletWinter",5000,-20,100, LibDefaults.DEF_PAT);
+        var heating = new ProcAirHeatCool.Builder().withName("heater").withInletFlow(winterInletFlow).build();
+        var expectedTout = 24.0;
+        var expectedX = winterInletFlow.getMoistAir().getX();
+        var expectedMda = winterInletFlow.getMassFlowDa();
+        var i1 = winterInletFlow.getMoistAir().getIx();
+        var i2 = LibPropertyOfAir.calc_Ma_Ix(expectedTout,expectedX,LibDefaults.DEF_PAT);
+        var expectedQ = (i2-i1)*expectedMda * 1000.0; //W
+
+        // ACT
+        heating.applyHeatingOutTxFromInQ(expectedQ);
+        var actualTout = heating.getOutletFlow().getMoistAir().getTx();
+        var actualX = heating.getOutletFlow().getMoistAir().getX();
+        var actualMda = heating.getOutletFlow().getMassFlowDa();
+        var actualQ = heating.getHeatQ();
+
+        // ASSERT
+        Assertions.assertEquals(expectedTout,actualTout);
+        Assertions.assertEquals(expectedX,actualX);
+        Assertions.assertEquals(expectedMda,actualMda);
+        Assertions.assertEquals(expectedQ,actualQ);
+
+    }
+
+   @Test
+   void applyHeatingInQFromOutTxTest(){
 
        // ARRANGE
        var winterInletFlow = FlowOfMoistAir.ofM3hVolFlow("InletWinter",5000,-20,100, LibDefaults.DEF_PAT);
-       var summerInletFlow = FlowOfMoistAir.ofM3hVolFlow("SummerWinter",5000,35,45, LibDefaults.DEF_PAT);
        var heating = new ProcAirHeatCool.Builder().withName("heater").withInletFlow(winterInletFlow).build();
        var expectedTout = 24.0;
        var expectedX = winterInletFlow.getMoistAir().getX();
@@ -62,7 +90,39 @@ public class ProcAirHeatCoolTests {
    }
 
    @Test
-   void applyCoolingInQFromOutTxTests(){
+   void applyHeatingInQOutTxFromOutRHTest(){
+
+        // ARRANGE
+        var winterInletFlow = FlowOfMoistAir.ofM3hVolFlow("InletWinter",5000,-20,100, LibDefaults.DEF_PAT);
+        var heating = new ProcAirHeatCool.Builder().withName("heater").withInletFlow(winterInletFlow).build();
+        var expectedTout = 24.0;
+        var expectedX = winterInletFlow.getMoistAir().getX();
+        var expectedMda = winterInletFlow.getMassFlowDa();
+        var i1 = winterInletFlow.getMoistAir().getIx();
+        var i2 = LibPropertyOfAir.calc_Ma_Ix(expectedTout,expectedX,LibDefaults.DEF_PAT);
+        var expectedQ = (i2-i1)*expectedMda * 1000.0; //W
+        var expectedRH = LibPropertyOfAir.calc_Ma_RH(expectedTout,expectedX,LibDefaults.DEF_PAT);
+
+        // ACT
+        heating.applyHeatingInQOutTxFromOutRH(expectedRH);
+        var actualTout = heating.getOutletFlow().getMoistAir().getTx();
+        var actualX = heating.getOutletFlow().getMoistAir().getX();
+        var actualMda = heating.getOutletFlow().getMassFlowDa();
+        var actualQ = heating.getHeatQ();
+        var actualRH = heating.getOutletFlow().getMoistAir().getRH();
+
+        // ASSERT
+        Assertions.assertEquals(expectedTout,actualTout,MATH_ACCURACY);
+        Assertions.assertEquals(expectedX,actualX);
+        Assertions.assertEquals(expectedMda,actualMda);
+        Assertions.assertEquals(expectedQ,actualQ,MATH_ACCURACY);
+        Assertions.assertEquals(expectedRH,actualRH,MATH_ACCURACY);
+
+    }
+
+   // COOLING
+   @Test
+   void applyCoolingInQFromOutTxTest(){
 
         // ARRANGE
         var summerInletFlow = FlowOfMoistAir.ofM3hVolFlow("SummerWinter",5000,35,45, LibDefaults.DEF_PAT);
@@ -88,5 +148,84 @@ public class ProcAirHeatCoolTests {
         Assertions.assertEquals(expectedRH,actualRH,MATH_ACCURACY);
 
     }
+
+   @Test
+   void applyCoolingOutTxFromInQTest(){
+
+       // ARRANGE
+       var summerInletFlow = FlowOfMoistAir.ofM3hVolFlow("SummerWinter",5000,35,45, LibDefaults.DEF_PAT);
+       var cooling = new ProcAirHeatCool.Builder().withName("cooler").withInletFlow(summerInletFlow).withCoolantTemps(3,6).build();
+       var expectedOutT = 11.0;
+       var expectedMda = summerInletFlow.getMassFlowDa();
+       var expectedTm = cooling.getAvrgWallTemp();
+       var result = LibPsychroProcess.calcCoolingInQFromOutTx(summerInletFlow,expectedTm,expectedOutT);
+       var expectedHeatQ = result[0];
+       var expectedX = result[2];
+       var expectedCondTemp = result[3];
+       var expectedCondMassFlow = result[4];
+       var expectedRH = LibPropertyOfAir.calc_Ma_RH(expectedOutT,expectedX,LibDefaults.DEF_PAT);
+
+       // ACT
+       cooling.applyCoolingOutTxFromInQ(expectedHeatQ);
+       var actualHeatQ = cooling.getHeatQ();
+       var actualOutT = cooling.getOutletFlow().getMoistAir().getTx();
+       var actualOutX = cooling.getOutletFlow().getMoistAir().getX();
+       var actualRH = cooling.getOutletFlow().getMoistAir().getRH();
+       var actualCOndTemp = cooling.getCondensateFlow().getFluid().getTx();
+       var actualCondMFlow = cooling.getCondensateFlow().getMassFlow();
+       var actualTm = cooling.getAvrgWallTemp();
+       var actualMda = cooling.getOutletFlow().getMassFlowDa();
+
+       // ASSERT
+       Assertions.assertEquals(expectedHeatQ,actualHeatQ);
+       Assertions.assertEquals(expectedOutT,actualOutT,MATH_ACCURACY);
+       Assertions.assertEquals(expectedX,actualOutX);
+       Assertions.assertEquals(expectedMda,actualMda);
+       Assertions.assertEquals(expectedRH,actualRH);
+       Assertions.assertEquals(expectedCondTemp,actualCOndTemp);
+       Assertions.assertEquals(expectedCondMassFlow,actualCondMFlow);
+       Assertions.assertEquals(expectedTm,actualTm);
+       Assertions.assertEquals(expectedTm,actualCOndTemp);
+
+   }
+
+   @Test
+   void applyCoolingInQFromOutRHTest(){
+       // ARRANGE
+       var summerInletFlow = FlowOfMoistAir.ofM3hVolFlow("SummerWinter",5000,35,45, LibDefaults.DEF_PAT);
+       var cooling = new ProcAirHeatCool.Builder().withName("cooler").withInletFlow(summerInletFlow).withCoolantTemps(3,6).build();
+       var expectedOutT = 11.0;
+       var expectedMda = summerInletFlow.getMassFlowDa();
+       var expectedTm = cooling.getAvrgWallTemp();
+       var result = LibPsychroProcess.calcCoolingInQFromOutTx(summerInletFlow,expectedTm,expectedOutT);
+       var expectedHeatQ = result[0];
+       var expectedX = result[2];
+       var expectedCondTemp = result[3];
+       var expectedCondMassFlow = result[4];
+       var expectedRH = LibPropertyOfAir.calc_Ma_RH(expectedOutT,expectedX,LibDefaults.DEF_PAT);
+
+       // ACT
+       cooling.applyCoolingInQFromOutRH(expectedRH);
+       var actualHeatQ = cooling.getHeatQ();
+       var actualOutT = cooling.getOutletFlow().getMoistAir().getTx();
+       var actualOutX = cooling.getOutletFlow().getMoistAir().getX();
+       var actualRH = cooling.getOutletFlow().getMoistAir().getRH();
+       var actualCOndTemp = cooling.getCondensateFlow().getFluid().getTx();
+       var actualCondMFlow = cooling.getCondensateFlow().getMassFlow();
+       var actualTm = cooling.getAvrgWallTemp();
+       var actualMda = cooling.getOutletFlow().getMassFlowDa();
+
+       // ASSERT
+       Assertions.assertEquals(expectedHeatQ,actualHeatQ, MATH_ACCURACY);
+       Assertions.assertEquals(expectedOutT,actualOutT, MATH_ACCURACY);
+       Assertions.assertEquals(expectedX,actualOutX, MATH_ACCURACY);
+       Assertions.assertEquals(expectedMda,actualMda);
+       Assertions.assertEquals(expectedRH,actualRH, MATH_ACCURACY);
+       Assertions.assertEquals(expectedCondTemp,actualCOndTemp);
+       Assertions.assertEquals(expectedCondMassFlow,actualCondMFlow, MATH_ACCURACY);
+       Assertions.assertEquals(expectedTm,actualTm);
+       Assertions.assertEquals(expectedTm,actualCOndTemp);
+
+   }
 
 }
