c108debe3672f3dc87c363ec580f8be87d164c2a
Piotr Jażdżyk <piotr.jazdzyk@o2.pl>
2022-11-08 14:42:04 +0100

    refactor LibPhysicsMixingTests, add display name, change assertions to assertJ

--- src/test/java/io/github/pjazdzyk/hvaclib/physics/LibPhysicsMixingTests.java
+++ src/test/java/io/github/pjazdzyk/hvaclib/physics/LibPhysicsMixingTests.java
@@ -2,49 +2,49 @@ package io.github.pjazdzyk.hvaclib.physics;
 
 import io.github.pjazdzyk.hvaclib.flows.FlowOfMoistAir;
 import io.github.pjazdzyk.hvaclib.common.Defaults;
-import io.github.pjazdzyk.hvaclib.physics.PhysicsOfAir;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.github.pjazdzyk.hvaclib.physics.PhysicsOfAirMixing.calcMixing;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class LibPhysicsMixingTests {
 
     @Test
-    public void calcMixingTest() {
-
+    @DisplayName("should return results for mixing of two different moist air flows")
+    public void calcMixing_shouldReturnResultsForMixingOfTwoDifferentMoistAirFlows() {
         //Arrange
         var Pat = Defaults.DEF_PAT;
-        var ta1 = -20.0; //oC
-        var RH1 = 100.0;
-        var vDa1 = 5000; //m3/h
-        var ta2 = 18.0;
-        var RH2 = 55;
-        var vDa2 = 5000; //m3/h
-        var firstFlow = FlowOfMoistAir.ofM3hVolFlow(vDa1, ta1, RH1, Pat);
-        var secondFlow = FlowOfMoistAir.ofM3hVolFlow(vDa2, ta2, RH2, Pat);
-        var expectedMda1 = firstFlow.getMassFlowDa();
-        var expectedMda2 = secondFlow.getMassFlowDa();
-        var expectedMda3 = expectedMda1 + expectedMda2;
-        var expectedOutX = (expectedMda1 * firstFlow.getX() + expectedMda2 * secondFlow.getX()) / expectedMda3;
-        var expectedOutI = (expectedMda1 * firstFlow.getMoistAir().getIx() + expectedMda2 * secondFlow.getMoistAir().getIx()) / expectedMda3;
-        var expectedOutTx = PhysicsOfAir.calcMaTaIX(expectedOutI, expectedOutX, Pat);
+        var firstFlowAirTemp = -20.0; //oC
+        var firstFlowRH = 100.0; // %
+        var firstDryAirFlow = 5000; //m3/h
+        var secondFlowAirTemp = 18.0; // oC
+        var secondFlowRH = 55; // %
+        var secondAirFlow = 5000; //m3/h
+        var firstFlowOfAir = FlowOfMoistAir.ofM3hVolFlow(firstDryAirFlow, firstFlowAirTemp, firstFlowRH, Pat);
+        var secondFlowOfAir = FlowOfMoistAir.ofM3hVolFlow(secondAirFlow, secondFlowAirTemp, secondFlowRH, Pat);
+
+        var expectedFirstFlowDryAirMassFlow = firstFlowOfAir.getMassFlowDa();
+        var expectedSecondFlowDryAirMassFlow = secondFlowOfAir.getMassFlowDa();
+        var expectedOutDryAirMassFlow = expectedFirstFlowDryAirMassFlow + expectedSecondFlowDryAirMassFlow;
+        var expectedOutHumidityRatio = (expectedFirstFlowDryAirMassFlow * firstFlowOfAir.getX() + expectedSecondFlowDryAirMassFlow * secondFlowOfAir.getX()) / expectedOutDryAirMassFlow;
+        var expectedOutEnthalpy = (expectedFirstFlowDryAirMassFlow * firstFlowOfAir.getMoistAir().getIx() + expectedSecondFlowDryAirMassFlow * secondFlowOfAir.getMoistAir().getIx()) / expectedOutDryAirMassFlow;
+        var expectedOutAirTemp = PhysicsOfAir.calcMaTaIX(expectedOutEnthalpy, expectedOutHumidityRatio, Pat);
 
         //Act
-        var result = calcMixing(firstFlow, secondFlow);
-        var actualMda1 = result.inMda();
-        var actualMda2 = result.recMda();
-        var actualMda3 = result.outMda();
-        var actualOutT = result.outTx();
-        var actualOutX = result.outX();
+        var mixingResults = calcMixing(firstFlowOfAir, secondFlowOfAir);
+        var actualFirstDryAirMassFlow = mixingResults.inMda();
+        var actualSecondDryAirMassFlow = mixingResults.recMda();
+        var actualOutDryAirMassFlow = mixingResults.outMda();
+        var actualOutAirTemp = mixingResults.outTx();
+        var actualOutHumidityRatio = mixingResults.outX();
 
         //Assert
-        Assertions.assertEquals(actualMda1, expectedMda1);
-        Assertions.assertEquals(actualMda2, expectedMda2);
-        Assertions.assertEquals(actualMda3, expectedMda3);
-        Assertions.assertEquals(actualOutX, expectedOutX);
-        Assertions.assertEquals(actualOutT, expectedOutTx);
-
+        assertThat(actualFirstDryAirMassFlow).isEqualTo(expectedFirstFlowDryAirMassFlow);
+        assertThat(actualSecondDryAirMassFlow).isEqualTo(expectedSecondFlowDryAirMassFlow);
+        assertThat(actualOutDryAirMassFlow).isEqualTo(expectedOutDryAirMassFlow);
+        assertThat(actualOutHumidityRatio).isEqualTo(expectedOutHumidityRatio);
+        assertThat(actualOutAirTemp).isEqualTo(expectedOutAirTemp);
     }
 
 }
