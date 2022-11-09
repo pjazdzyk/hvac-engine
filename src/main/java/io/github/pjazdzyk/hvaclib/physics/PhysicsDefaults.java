c3d2d7d4739b665c23df8dd9557c717b53e9fa64
Piotr Jażdżyk <piotr.jazdzyk@o2.pl>
2022-11-09 10:40:41 +0100

    removed unused specific heat value

--- src/main/java/io/github/pjazdzyk/hvaclib/physics/PhysicsDefaults.java
+++ src/main/java/io/github/pjazdzyk/hvaclib/physics/PhysicsDefaults.java
@@ -5,13 +5,10 @@ public class PhysicsDefaults {
     private PhysicsDefaults() {
     }
 
-    // Default air core property values
     public final static double DEF_PAT = 101_325;                   // [Pa]                 - Standard atmospheric pressure (physical atmosphere)
     public final static double DEF_AIR_TEMP = 20.0;                 // [oC]                 - default air temperature oC
     public final static double DEF_AIR_RH = 50.0;                   // [%]                  - default relative humidity in %
     public final static double DEF_ASL_ELEV = 0.0;                  // [m]                  - default elevation above the sea level
-    // Default properties
-    public final static double DEF_WT_CP = 4.190;                   // [KJ/(kg*K)]          - Water specific heat
     public final static double DEF_WT_TW = 10.0;                    // [oC]                 - Water temperature (default)
     public final static double DEF_ICE_CP = 2.09;                   // [kJ/(kg*K)]          - Ice specific heat
 
