2acb3d7ac109b0c03bfa3609f888c24d54ae2072
Piotr Jażdżyk <piotr.jazdzyk@o2.pl>
2022-11-13 12:43:39 +0100

    add PropertyPhysicsArgumentException

---
+++ src/main/java/io/github/pjazdzyk/hvaclib/properties/exceptions/PropertyPhysicsArgumentException.java
@@ -0,0 +1,8 @@
+package io.github.pjazdzyk.hvaclib.properties.exceptions;
+
+public class PropertyPhysicsArgumentException extends RuntimeException {
+    public PropertyPhysicsArgumentException(String msg) {
+        super(msg);
+    }
+
+}
