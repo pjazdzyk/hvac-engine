133d2230861f341e63d5b8c56a46f6252c800307
Piotr Jażdżyk <piotr.jazdzyk@o2.pl>
2022-11-13 17:35:22 +0100

    substract FlowDefaults

---
+++ src/main/java/io/github/pjazdzyk/hvaclib/flows/FlowDefaults.java
@@ -0,0 +1,7 @@
+package io.github.pjazdzyk.hvaclib.flows;
+
+public class FlowDefaults {
+
+    public static final String DEF_FLOW_NAME = "Generic flow";       // -                    - Default flow name
+    public static final double DEF_MASS_FLOW = 0.1;                  // kg/s                 - Default mass flow
+}
