ce88dae225182fea3358384cb76a451e8298d3b2
Piotr Jażdżyk <piotr.jazdzyk@o2.pl>
2022-11-13 12:45:22 +0100

    removed unused constructor, relocate to new package structure

--- src/main/java/io/github/pjazdzyk/hvaclib/process/exceptions/ProcessArgumentException.java
+++ src/main/java/io/github/pjazdzyk/hvaclib/process/exceptions/ProcessArgumentException.java
@@ -1,11 +1,6 @@
 package io.github.pjazdzyk.hvaclib.process.exceptions;
 
 public class ProcessArgumentException extends RuntimeException {
-
-    public ProcessArgumentException() {
-        super();
-    }
-
     public ProcessArgumentException(String msg) {
 
         super(msg);
