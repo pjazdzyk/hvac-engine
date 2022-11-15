59df45e2159aad6ebf18bd61f8513b478a50b619
Piotr Jażdżyk <piotr.jazdzyk@o2.pl>
2022-11-15 15:12:05 +0100

    remove unnecessary getter

--- src/main/java/io/github/pjazdzyk/hvaclib/flows/FlowOfHumidGas.java
+++ src/main/java/io/github/pjazdzyk/hvaclib/flows/FlowOfHumidGas.java
@@ -3,8 +3,6 @@ package io.github.pjazdzyk.hvaclib.flows;
 import io.github.pjazdzyk.hvaclib.fluids.HumidGas;
 
 public interface FlowOfHumidGas extends FlowOfFluid<HumidGas> {
-    HumidGas getHumidGas();
-
     double getMassFlowDa();
 
     double getVolFlowDa();
