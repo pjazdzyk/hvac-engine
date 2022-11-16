cf151e44b6415c21f218125acf7504b2743a4c52
Piotr Jażdżyk <piotr.jazdzyk@o2.pl>
2022-11-16 18:56:44 +0100

    add MathUtils unit tests

---
+++ src/test/java/io/github/pjazdzyk/hvaclib/common/MathUtilsTest.java
@@ -0,0 +1,40 @@
+package io.github.pjazdzyk.hvaclib.common;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MathUtilsTest {
+
+    @Test
+    void calcArithmeticAverage() {
+        // Arrange
+        var a = 2;
+        var b = 6;
+        var c = 4;
+        var expectedAverage = 4;
+
+        // Act
+        var actualAverage = MathUtils.calcArithmeticAverage(a, b, c);
+
+        // Assert
+        assertThat(actualAverage).isEqualTo(expectedAverage);
+    }
+
+    @Test
+    void linearInterpolation() {
+        // Arrange
+        var x1 = 100;
+        var f_x1 = 10;
+        var x2 = 200;
+        var f_x2 = 20;
+        var x = 150;
+        var expectedInterpolatedValue = 15;
+
+        // Act
+        var actualInterpolatedValue = MathUtils.linearInterpolation(x1, f_x1, x2, f_x2, x);
+
+        // Assert
+        assertThat(actualInterpolatedValue).isEqualTo(expectedInterpolatedValue);
+    }
+}
