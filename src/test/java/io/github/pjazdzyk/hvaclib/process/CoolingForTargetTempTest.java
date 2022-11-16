ca1ba065f153fa488310155f9d623078e40edfd5
Piotr Jażdżyk <piotr.jazdzyk@o2.pl>
2022-11-16 21:06:21 +0100

    add cooling process unit test

---
+++ src/test/java/io/github/pjazdzyk/hvaclib/process/CoolingForTargetTempTest.java
@@ -0,0 +1,65 @@
+package io.github.pjazdzyk.hvaclib.process;
+
+import io.github.pjazdzyk.hvaclib.PhysicsTestConstants;
+import io.github.pjazdzyk.hvaclib.flows.FlowOfFluid;
+import io.github.pjazdzyk.hvaclib.flows.FlowOfHumidGas;
+import io.github.pjazdzyk.hvaclib.flows.FlowOfMoistAir;
+import io.github.pjazdzyk.hvaclib.fluids.HumidGas;
+import io.github.pjazdzyk.hvaclib.fluids.LiquidWater;
+import io.github.pjazdzyk.hvaclib.fluids.MoistAir;
+import io.github.pjazdzyk.hvaclib.fluids.PhysicsPropOfMoistAir;
+import io.github.pjazdzyk.hvaclib.fluids.PhysicsPropOfWater;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CoolingForTargetTempTest implements PhysicsTestConstants {
+
+    @Test
+    @DisplayName("should cool down inlet air when target temperature is given")
+    void runProcess_shouldCoolDownInletAir_whenTargetTempIsGiven() {
+        // Arrange
+        HumidGas coolingCaseInletAir = new MoistAir.Builder()
+                .withAtmPressure(P_TEST)
+                .withAirTemperature(34.0)
+                .withRelativeHumidity(40.0)
+                .build();
+        FlowOfHumidGas inletFlow = new FlowOfMoistAir.Builder(coolingCaseInletAir)
+                .withMassFlowMa(1.0)
+                .build();
+        var expectedOutAirTemp = 17.0; // oC
+        var expectedOutHumRatio = 0.009903615645455723; // kg.wv/kg.da
+        var inletHumidGasMassFlow = inletFlow.getMassFlowDa();
+        var expectedByPassFactor = PhysicsOfCooling.calcCoolingCoilBypassFactor(TYPICAL_AVERAGE_COIL_WALL_TEMP, coolingCaseInletAir.getTemp(), expectedOutAirTemp);
+        var directContactFlow = (1.0 - expectedByPassFactor) * inletHumidGasMassFlow;
+        var inletHumRatio = coolingCaseInletAir.getHumRatioX();
+        var inletSpecificEnthalpy = coolingCaseInletAir.getSpecEnthalpy();
+        var saturationPressureAtArvWallTemp = PhysicsPropOfMoistAir.calcMaPs(TYPICAL_AVERAGE_COIL_WALL_TEMP);
+        var humRatioAtAvrWallTemp = PhysicsPropOfMoistAir.calcMaXMax(saturationPressureAtArvWallTemp, P_TEST);
+        var specificEnthalpyAtAvrWallTemp = PhysicsPropOfMoistAir.calcMaIx(TYPICAL_AVERAGE_COIL_WALL_TEMP, humRatioAtAvrWallTemp, P_TEST);
+        var expectedCondensateTemp = TYPICAL_AVERAGE_COIL_WALL_TEMP;
+
+        // Act
+        ProcessWithCondensate coolingProcess = new CoolingForTargetTemp(inletFlow, TYPICAL_AVERAGE_COIL_WALL_TEMP, expectedOutAirTemp);
+        FlowOfHumidGas actualResultingFlow = coolingProcess.runProcess();
+        var resultingAirState = actualResultingFlow.getFluid();
+        var actualOutTemp = resultingAirState.getTemp();
+        var actualHumRatio = resultingAirState.getHumRatioX();
+        FlowOfFluid<LiquidWater> condensateFlow = coolingProcess.getCondensateFlow();
+        var actualCondensateTemp = condensateFlow.getFluid().getTemp();
+        var actualCondensateFlow = condensateFlow.getMassFlow();
+        var actualHeatOfProcess = coolingProcess.getHeatOfProcess();
+        var condensateSpecificEnthalpy = PhysicsPropOfWater.calcIx(actualCondensateTemp);
+
+        // Assert
+        var expectedCondensateFlow = PhysicsOfCooling.calcCondensateDischarge(directContactFlow, inletHumRatio, humRatioAtAvrWallTemp);
+        var expectedHeatOfProcess = (directContactFlow * (specificEnthalpyAtAvrWallTemp - inletSpecificEnthalpy) + actualCondensateFlow * condensateSpecificEnthalpy) * 1000;
+        assertThat(actualHeatOfProcess).isEqualTo(expectedHeatOfProcess);
+        assertThat(actualOutTemp).isEqualTo(expectedOutAirTemp);
+        assertThat(actualHumRatio).isEqualTo(expectedOutHumRatio);
+        assertThat(actualCondensateTemp).isEqualTo(expectedCondensateTemp);
+        assertThat(actualCondensateFlow).isEqualTo(expectedCondensateFlow);
+    }
+
+}
