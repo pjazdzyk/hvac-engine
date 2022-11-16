890be9479f85d39a1d0eba2cd1295d15fee22b35
Piotr Jażdżyk <piotr.jazdzyk@o2.pl>
2022-11-16 11:58:16 +0100

    add humidity ratio as field to heating dto results, to ambiguously define thermodynamic state of air

--- src/main/java/io/github/pjazdzyk/hvaclib/process/resultsdto/HeatingResultDto.java
+++ src/main/java/io/github/pjazdzyk/hvaclib/process/resultsdto/HeatingResultDto.java
@@ -2,5 +2,6 @@ package io.github.pjazdzyk.hvaclib.process.resultsdto;
 
 public record HeatingResultDto(double pressure,
                                double heatOfProcess,
-                               double outTemperature){
+                               double outTemperature,
+                               double outHumidityRatio) {
 }
