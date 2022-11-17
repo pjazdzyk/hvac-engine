5e1af42aa0d1245dd76e825bbc1e3966f3357065
Piotr Jażdżyk <piotr.jazdzyk@o2.pl>
2022-11-17 13:50:16 +0100

    add toString() method

--- src/main/java/io/github/pjazdzyk/hvaclib/process/GenericCoolingProcess.java
+++ src/main/java/io/github/pjazdzyk/hvaclib/process/GenericCoolingProcess.java
@@ -4,10 +4,11 @@ import io.github.pjazdzyk.hvaclib.flows.FlowOfFluid;
 import io.github.pjazdzyk.hvaclib.flows.FlowOfHumidGas;
 import io.github.pjazdzyk.hvaclib.fluids.LiquidWater;
 
+import java.util.Objects;
+
 abstract class GenericCoolingProcess extends GenericHeatingProcess implements ProcessWithCondensate {
 
     protected FlowOfFluid<LiquidWater> condensateFlow;
-
     protected double averageCoilWallTemp;
 
     protected GenericCoolingProcess(FlowOfHumidGas inletFlow, double averageCoilWallTemp) {
@@ -23,6 +24,20 @@ abstract class GenericCoolingProcess extends GenericHeatingProcess implements Pr
     @Override
     public double getAverageCoilWallTemp(){
         return averageCoilWallTemp;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder bld = createStringBuilderFromInletAndOutlet();
+        if (Objects.nonNull(condensateFlow)) {
+            bld.append("CONDENSATE FLOW DATA:\n");
+            bld.append(condensateFlow).append("\n");
+        }
+        bld.append("PROCESS SPECIFIC DATA:\n");
+        bld.append(String.format("Q = %.3f W (heat of process)\n", heatOfProcess));
+        bld.append(String.format("Q = %.2f KW (heat of process)\n", heatOfProcess/1000d));
+        bld.append(String.format("tm.wall = %.3f oC (average cooling coil wall temperature)", averageCoilWallTemp));
+        return bld.toString();
     }
 
 }
--- src/main/java/io/github/pjazdzyk/hvaclib/process/GenericHeatingProcess.java
+++ src/main/java/io/github/pjazdzyk/hvaclib/process/GenericHeatingProcess.java
@@ -2,7 +2,9 @@ package io.github.pjazdzyk.hvaclib.process;
 
 import io.github.pjazdzyk.hvaclib.flows.FlowOfHumidGas;
 
-abstract class GenericHeatingProcess implements ProcessHeatDriven{
+import java.util.Objects;
+
+abstract class GenericHeatingProcess implements ProcessHeatDriven {
     protected final FlowOfHumidGas inletFlow;
     protected FlowOfHumidGas outletFlow;
 
@@ -26,6 +28,28 @@ abstract class GenericHeatingProcess implements ProcessHeatDriven{
     @Override
     public double getHeatOfProcess() {
         return heatOfProcess;
+    }
+
+    protected StringBuilder createStringBuilderFromInletAndOutlet(){
+        String title = String.format("-------------- PROCESS TYPE: %s --------------",this.getClass().getSimpleName());
+        StringBuilder bld = new StringBuilder();
+        bld.append(title).append("\n");
+        bld.append("INLET FLOW DATA:\n");
+        bld.append(inletFlow).append("\n");
+        if (Objects.nonNull(outletFlow)) {
+            bld.append("OUTLET FLOW DATA:\n");
+            bld.append(outletFlow).append("\n");
+        }
+        return bld;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder bld = createStringBuilderFromInletAndOutlet();
+        bld.append("PROCESS SPECIFIC DATA:\n");
+        bld.append(String.format("Q = %.3f W (heat of process) \n", heatOfProcess));
+        bld.append(String.format("Q = %.2f kW (heat of process)", heatOfProcess/1000d));
+        return bld.toString();
     }
 
 
