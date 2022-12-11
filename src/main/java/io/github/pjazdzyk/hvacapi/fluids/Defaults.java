56ce6799178c4d4c94a5001739d2a2683f06d124
Piotr Jażdżyk <piotr.jazdzyk@o2.pl>
2022-12-11 16:46:13 +0100

    add Defaults class

---
+++ src/main/java/io/github/pjazdzyk/hvacapi/fluids/Defaults.java
@@ -0,0 +1,9 @@
+package io.github.pjazdzyk.hvacapi.fluids;
+
+public class Defaults {
+
+    private Defaults() {}
+
+    public final static double DEF_PAT = 101_325;                   // [Pa]                 - Standard atmospheric pressure (physical atmosphere)
+
+}
