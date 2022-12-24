baa2f94240462ab8af6222a6476250e53e95eb78
Piotr Jażdżyk <piotr.jazdzyk@o2.pl>
2022-12-24 23:45:34 +0100

    add new draft starting page and associated syles, add Synerset and Harvard logos and icons

--- src/main/resources/static/js/navbar.js
+++ src/main/resources/static/js/navbar.js
@@ -3,7 +3,5 @@ import {
   registerToggleClickAction
 } from './sharedScripts.js';
 
-// CONTEXT CALLS
-
 // Burger menu button toggle: show and hide menu items
 registerToggleClickAction("toggle-button", "navi-list", "active");
