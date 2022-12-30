219a484f212e522601dae4e85f307c8a644fcde3
Piotr Jażdżyk <piotr.jazdzyk@o2.pl>
2022-12-30 21:45:33 +0100

    add moist air and water property forms, add scripts for calculate and reset buttons, added select all on focus. Calculate method uses REST API to generate the results.

--- src/main/resources/static/js/shared-scripts.js
+++ src/main/resources/static/js/shared-scripts.js
@@ -1,9 +1,3 @@
-const serviceHost = "localhost";
-const servicePort = "8002";
-const apiVersion = "api/v1";
-const moistAirEndpoint = "properties/air"
-const restApiMoistAirUrl = `http://${serviceHost}:${servicePort}/${apiVersion}/${moistAirEndpoint}`; 
-
 export function clearField(inputField) {
   inputField.value = "";
 }
@@ -23,6 +17,7 @@ export function registerFocusAction(sourceElementId, action) {
   choiceBox.addEventListener("foucs", action);
 }
 
+
 export function registerFocusForInputsOfType(sourceElementId, inputElementType) {
   let form = document.getElementById(sourceElementId);
   let inputs = form.getElementsByTagName(inputElementType);
@@ -33,4 +28,59 @@ export function registerFocusForInputsOfType(sourceElementId, inputElementType) 
   }
 }
 
-console.log(restApiMoistAirUrl);
+export function getAllActiveFieldsFromFormAsMap(formId) {
+  let form = document.getElementById(formId);
+  let inputsMap = new Map();
+  for (let i = 0; i < form.elements.length; i++) {
+    let input = form.elements[i];
+    if (input.tagName === 'INPUT' && !input.readOnly && !input.disabled) {
+      inputsMap.set(input.id, input.value);
+    }
+  }
+  return inputsMap;
+}
+
+export function getAllInactiveFieldsAsList(formId) {
+  let form = document.getElementById(formId);
+  let inputs = [];
+  for (let i = 0; i < form.elements.length; i++) {
+    let input = form.elements[i];
+    if (input.tagName === 'INPUT' && (input.readOnly || input.disabled)) {
+      inputs.push(input);
+    }
+  }
+  return inputs;
+}
+
+export function clearAllInactiveFields(formId){
+  let inputs = getAllInactiveFieldsAsList(formId);
+  for (const element of inputs) {
+    element.value = "";
+  }
+}
+
+export async function makeGetApiCall(baseUrl, params) {
+  let queryParams = new URLSearchParams(params);
+  let url = `${baseUrl}?${queryParams.toString()}`;
+  let callMode = {
+    method: "GET",
+    mode: "cors"
+  }
+  let response = await fetch(url, callMode);
+  if (response.status !== 200) {
+    throw new Error(`Failed to load API: ${response.status}`);
+  }
+  
+  return await response.json();
+}
+
+export function updateFormFromJson(formId, data) {
+  let form = document.getElementById(formId);
+  let inputs = form.getElementsByTagName('INPUT');
+  for (const element of inputs) {
+    let inputId = element.id;
+    if (data.hasOwnProperty(inputId)) {
+      element.value = data[inputId];
+    }
+  }
+}
