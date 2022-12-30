bfec2f3c6d27441f49edc46491df99bfe2e2f530
Piotr Jażdżyk <piotr.jazdzyk@o2.pl>
2022-12-30 21:44:17 +0100

    added cross origin support, refactoring

--- src/main/java/io/github/pjazdzyk/hvacapi/infrastructure/controllers/fluids/FluidRestController.java
+++ src/main/java/io/github/pjazdzyk/hvacapi/infrastructure/controllers/fluids/FluidRestController.java
@@ -3,12 +3,14 @@ package io.github.pjazdzyk.hvacapi.infrastructure.controllers.fluids;
 import io.github.pjazdzyk.hvacapi.fluids.FluidService;
 import io.github.pjazdzyk.hvacapi.fluids.dto.FluidResponseDto;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/v1/properties")
 public class FluidRestController {
 
--- src/main/java/io/github/pjazdzyk/hvacapi/infrastructure/controllers/fluids/MoistAirRestController.java
+++ src/main/java/io/github/pjazdzyk/hvacapi/infrastructure/controllers/fluids/MoistAirRestController.java
@@ -6,6 +6,7 @@ import io.github.pjazdzyk.hvacapi.fluids.dto.MoistAirDto;
 import io.github.pjazdzyk.hvacapi.fluids.dto.MoistAirResponseDto;
 import io.github.pjazdzyk.hvacapi.infrastructure.controllers.fluids.exeptions.InvalidPropertyArgumentException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Objects;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/v1/properties")
 public class MoistAirRestController {
 
@@ -24,18 +26,18 @@ public class MoistAirRestController {
     }
 
     @GetMapping("/air")
-    public ResponseEntity<MoistAirResponseDto> getMoistAirProperties(@RequestParam double dryBulbTemp,
+    public ResponseEntity<MoistAirResponseDto> getMoistAirProperties(@RequestParam double dryBulbTemperature,
                                                                      @RequestParam(required = false) Double relativeHumidity,
-                                                                     @RequestParam(required = false) Double humidityRatio,
+                                                                     @RequestParam(required = false) Double humidityRatioX,
                                                                      @RequestParam(required = false, defaultValue = FluidsDefaults.DEF_PAT + "") Double absPressure) {
 
-        if (Objects.isNull(relativeHumidity) && Objects.isNull(humidityRatio)) {
+        if (Objects.isNull(relativeHumidity) && Objects.isNull(humidityRatioX)) {
             throw new InvalidPropertyArgumentException("You have to specify at least one type of humidity");
         }
-        double humidity = Objects.isNull(humidityRatio)
-                ? fluidService.convertRHtoHumRatio(absPressure, dryBulbTemp, relativeHumidity)
-                : humidityRatio;
-        MoistAirDto moistAirDto = new MoistAirDto(absPressure, dryBulbTemp, humidity);
+        double humidity = Objects.isNull(humidityRatioX)
+                ? fluidService.convertRHtoHumRatio(absPressure, dryBulbTemperature, relativeHumidity)
+                : humidityRatioX;
+        MoistAirDto moistAirDto = new MoistAirDto(absPressure, dryBulbTemperature, humidity);
         MoistAirResponseDto moistAirResponseDto = fluidService.createMoistAirProperty(moistAirDto);
         return ResponseEntity.ok(moistAirResponseDto);
     }
